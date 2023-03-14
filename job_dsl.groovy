33e0229492c7d720a38aa5d96b48fb0b315fcc36
badreddine <Badreddineqassimi@gmail.com>
2023-03-14 14:02:30 +0100

    EH

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,26 +1,48 @@
-library 'jenkins.jobdsl.core.dsl'
-library 'jenkins.jobdsl.dsl.helpers.publisher'
+folder('/Tools') {
+    description('Folder for miscellaneous tools.')
+    displayName('Tools')
+}
 
-pipelineJob('my-pipeline-job') {
-    definition {
-        cps {
-            script('''
-                pipeline {
-                    agent any
-                    stages {
-                        stage('one') {
-                            steps {
-                                shell('df')
-                            }
-                            steps{
-                                shell('echo "Hello dear $NAME!"
-                                      date
-                                      echo "This is your DDoS number $BUILD_NUMBER."')
-                            }
-                        }
+freeStyleJob('/Tools/clone-repository') {
+    wrappers {
+        preBuildCleanup {
+            preBuildCleanup()
+        }
+    }
+    parameters {
+        stringParam("GIT_REPOSITORY_URL", "", "Git URL of the repository to clone")
+    }
+    steps {
+        shell("git clone \$GIT_REPOSITORY_URL")
+    }
+}
+
+freeStyleJob('/Tools/SEED') {
+    parameters {
+        stringParam("GITHUB_NAME", "", "GitHub repository owner/repo_name (e.g.: \"EpitechIT31000/chocolatine\")")
+        stringParam("DISPLAY_NAME", "", "Display name for the job")
+    }
+    scm {
+        github('\$GITHUB_NAME')
+        triggers {
+            cron("* * * * *")
+        }
+    }
+    steps {
+        dsl {
+            text('''job ("\$DISPLAY_NAME") {
+                wrappers {
+                    preBuildCleanup {
+                        preBuildCleanup()
                     }
                 }
-            ''')
+                steps {
+                    shell("make fclean")
+                    shell("make")
+                    shell("make test")
+                    shell("make clean")
+                }
+            }'''.stripIndent())
         }
     }
 }
